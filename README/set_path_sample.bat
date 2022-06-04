23b18fc2d6703e4716214f468d4cdcf750497171
Yasushi Ohsugi <70119444+Yasushi-Osugi@users.noreply.github.com>
2022-06-04 09:41:57 +0900

    Add files via upload

---
+++ README/set_path_sample.bat
@@ -0,0 +1,16 @@
+rem
+rem a sample setting PATH from anaconda_prompt to python
+rem
+
+
+rem change directory
+
+cd source
+
+
+rem path2python
+
+set PATH=%PATH%;C:\Users\xxxxxxxx\Anaconda3
+set PATH=%PATH%;C:\Users\xxxxxxxx\Anaconda3\Library\bin
+
+
